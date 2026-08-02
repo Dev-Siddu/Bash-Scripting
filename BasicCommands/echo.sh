040a8dac505b9c1b3af6e20808a9dfb1abcaf5a2
Siddu <siddarood005@gmail.com>
2026-08-02 08:58:51 +0530

    echo command

---
+++ BasicCommands/echo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# the above line is called a shebang which indicates with which engine this script will be executed
+# you can check with a command > which bash
+
+# script to print hello world to terminal
+
+echo "Hello World!...:)"
+
+# echo command prints the given input to the terminal. However there is an another use case also like when there are multiple client are connected to a same machine echo will print the message to all the connected terminals usally used in adminstartor to send message connected clients etc use cases you can assume like when admin rebooting system he/she can echo the message saying save and disconnect as we are rebooting the machine ...
---
+++ BasicCommands/echo_with_var.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# shabang like
+
+# Q) Write a bash script that takes a user's name as input and uses the echo command to greet the user with "Hello, [name]!".
+
+# print message to say input your name
+echo "Enter your name :"
+
+# read the user input and assign to variable name
+read name
+
+#Greet the user with there name
+echo "Hello, $name!"
+
+
